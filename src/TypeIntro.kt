73fff7b5ec3e2770d7691e4909f10690ad2b34bb
yusukeyusuke <yusukeyusuke@gmail.com>
2020-01-26 20:31:17 +0900

    challange commit

--- src/TypeIntro.kt
+++ src/TypeIntro.kt
@@ -1,7 +1,23 @@
+const val MAX_EXPERIENCE: Int = 5000
+const val PUB_NAME:String = "Unicorn's Horn"
+
 fun main() {
     val playerName = "Estragon"
-    var experiencePoints: Int = 5
+    var hasSteed:Boolean = false;
+    var experiencePoints = 10000
+    var publican_name:String = "unicorn man"
+    var gold:Int = 50
+    val STEEDS: List<String> = listOf("dragon", "minotaur")
+    val DRINKS: List<String> = listOf("mead", "wine", "LaCroix")
+
     experiencePoints += 5
+    if (experiencePoints > MAX_EXPERIENCE) {
+        experiencePoints = MAX_EXPERIENCE
+    }
+
+
+
     println(experiencePoints)
     println(playerName)
+    println(playerName.reversed())
 }
